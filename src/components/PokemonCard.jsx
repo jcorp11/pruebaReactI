181cbe379639faa58d40d365ddab00dd6236d1a8
jcorp11 <joaco.011@gmail.com>
2023-12-04 18:43:41 -0300

    cambios en la tarjeta

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,19 +1,34 @@
 const PokemonCard = ({ pokemon }) => {
   return (
-    <div>
-      <h2>{pokemon.name}</h2>
-      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-      <p>Height: {pokemon.height}</p>
-      <p>Weight: {pokemon.weight}</p>
+    <div className="card" style={{ width: "18rem", margin: "20px" }}>
+      <img
+        src={pokemon.sprites.front_default}
+        alt={pokemon.name}
+        className="card-img-top"
+      />
+      <div className="card-body">
+        <h5 className="card-title">{pokemon.name}</h5>
+        <p className="card-text">Height: {pokemon.height}</p>
+        <p className="card-text">Weight: {pokemon.weight}</p>
 
-      <h3>Stats:</h3>
-      <ul>
-        {pokemon.stats.map((stat) => (
-          <li key={stat.stat.name}>
-            {stat.stat.name}: {stat.base_stat}
-          </li>
-        ))}
-      </ul>
+        <h6>Abilities:</h6>
+        <ul className="list-group">
+          {pokemon.abilities.map((ability) => (
+            <li key={ability.ability.name} className="list-group-item">
+              {ability.ability.name}
+            </li>
+          ))}
+        </ul>
+
+        <h6>Stats:</h6>
+        <ul className="list-group">
+          {pokemon.stats.map((stat) => (
+            <li key={stat.stat.name} className="list-group-item">
+              {stat.stat.name}: {stat.base_stat}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
--- src/components/Selector.jsx
+++ src/components/Selector.jsx
@@ -2,30 +2,35 @@ import style from "../components/css/Selector.module.css";
 const Selector = ({ setPokemonType, setPokemonStat }) => {
   return (
     <>
-      <h1>Elige el tipo de Pokemon</h1>
       <section className={style.selectorContainer}>
-        <select
-          className="form-select"
-          onChange={(e) => setPokemonType(e.target.value)}
-        >
-          <option defaultValue={""}>Elige un tipo de pokemon</option>
-          <option value="electric">Electric</option>
-          <option value="fire">Fire</option>
-          <option value="water">Water</option>
-          <option value="steel">Steel</option>
-        </select>
-        <select
-          className="form-select"
-          onChange={(e) => setPokemonStat(e.target.value)}
-        >
-          <option defaultValue={""}>Elige una stat para ordenar</option>
-          <option value="attack">Attack</option>
-          <option value="hp">HP</option>
-          <option value="defense">Defense</option>
-          <option value="special-attack">Special attack</option>
-          <option value="special-defense">Special defense</option>
-          <option value="speed">Speed</option>
-        </select>
+        <section>
+          <h1>Elige el tipo de Pokemon</h1>
+          <select
+            className="form-select"
+            onChange={(e) => setPokemonType(e.target.value)}
+          >
+            <option defaultValue={""}>Elige un tipo de pokemon</option>
+            <option value="electric">Electric</option>
+            <option value="fire">Fire</option>
+            <option value="water">Water</option>
+            <option value="steel">Steel</option>
+          </select>
+        </section>
+        <section>
+          <h1>Elige la stat para ordenar</h1>
+          <select
+            className="form-select"
+            onChange={(e) => setPokemonStat(e.target.value)}
+          >
+            <option defaultValue={""}>Elige una stat para ordenar</option>
+            <option value="attack">Attack</option>
+            <option value="hp">HP</option>
+            <option value="defense">Defense</option>
+            <option value="special-attack">Special attack</option>
+            <option value="special-defense">Special defense</option>
+            <option value="speed">Speed</option>
+          </select>
+        </section>
       </section>
     </>
   );
